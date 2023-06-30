f93a5321d3e422b602ccc3f5d2a6f0ad79cc0c24
Ronaldyika <buhnyuyronald@gmail.com>
2023-07-01 00:36:01 +0100

    started with the real time chat app using channels

---
+++ real_time_chat/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('',views.frontpage,name='frontpage'),
+    path('signup/',views.SignUp,name='signup'),
+    path('login/',auth_views.LoginView.as_view(template_name='core/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout')
+]
